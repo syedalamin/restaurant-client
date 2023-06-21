22cd298ff733762515c89bab4b187dbc68c11d67
syedalamin <syedalamin2580@gmail.com>
2023-06-21 11:30:33 +0600

    banner

---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    const navOptions = <>
+    <li><Link to='/'>Our Menu</Link></li>
+    </>
+    return (
+        <div>
+            <div className="navbar max-w-screen-xl mx-auto fixed z-10 bg-opacity-30 text-white bg-black">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">SA Restaurant</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
